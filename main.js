eec2e7e10e37e123dceb406206bc5223197c1eb2
liadk07 <91607695+liad07@users.noreply.github.com>
2022-08-18 16:34:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+
+    function f() {
+        document.getElementById("web").innerHTML+=document.getElementById("html").value
+    }
+    function f1() {
+        document.getElementById("web").innerHTML+="<style>"+document.getElementById("css").value+"</style>"
+
+    }
+
